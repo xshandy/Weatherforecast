63cbff75b9901d98bbb95dd6871ac06ca705c4e9
xshandy <shandygshek@hotmail.co.uk>
2024-05-06 11:01:49 +0100

    injecting the icon API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function displayWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeELement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   console.log(response.data);
 
@@ -16,6 +17,7 @@ function displayWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 }
 
 function formatDate(date) {
